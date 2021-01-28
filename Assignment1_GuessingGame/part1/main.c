4fdf132e129741bb2c905eee7be1246bb8b6a021
erxian <evelynhsu21@gmail.com>
2021-01-28 06:51:53 -0500

    finish number guess game in Assignment1

--- Assignment1_GuessingGame/part1/main.c
+++ Assignment1_GuessingGame/part1/main.c
@@ -1,4 +1,37 @@
-// Add your program here!
-int main(){
-	printf("Hello World!\n");
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int round = 5;
+	int inputInteger;
+	int guessAbility[5];
+	for (int i = 0; i < round; i++) {
+		int randomInteger;
+		int guessTime = 1;
+		randomInteger = rand() % 10 + 1;
+		printf("==========================\n");
+		printf("CPU Says: Pick a number 1-10\n");
+		printf("==========================\n");
+		printf("Make a guess: ");
+		scanf("%d", &inputInteger);
+		while (inputInteger != randomInteger) {
+			if (inputInteger < randomInteger) {
+				printf("No guess higher!\n");
+			} else {
+				printf("No guess lower!\n");
+			}
+			guessTime++;
+			printf("Make a guess: ");
+			scanf("%d", &inputInteger);
+		}
+		printf("You got it!\n");
+		guessAbility[i] = guessTime;
+	}
+	printf("=================================================\n");
+	printf("|Here are the results of your guessing abilities|\n");
+	printf("=================================================\n");
+	for (int i = 0; i < round; i++) {
+		printf("Game %d took you %d guesses\n", i, guessAbility[i]);
+	}
+	return 0;
 }
