9160805c38a6c278e7066e7dd74f5dd0288bfc3a
erxian <evelynhsu21@gmail.com>
2021-01-28 05:22:00 -0500

    finish power function in Lab1

--- Lab1_UnixTerminal/main.c
+++ Lab1_UnixTerminal/main.c
@@ -1,1 +1,17 @@
-// Add your program here!
+#include <stdio.h>
+
+double power(double base, double n) {
+	double p = 1;
+	for(int i = 0; i < n; i++) {
+		p *= base;
+	}
+	return p;
+}
+
+int main() {
+	double num1 = power(2, 1);
+	double num2 = power(2, 10);
+	printf("1 to the power 2 is: %f\n", num1);
+	printf("10 to the power 2 is: %f\n", num2);
+	return 0;
+}
