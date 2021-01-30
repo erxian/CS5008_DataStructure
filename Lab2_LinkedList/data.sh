6fd4be45a09aa0f639fd0e72880a56d0ede860c3
erxian <evelynhsu21@gmail.com>
2021-01-30 16:51:15 +0800

    exercise data.sh in lab1

--- Lab2_LinkedList/data.sh
+++ Lab2_LinkedList/data.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 # Use this script to scrape data from the web using the provided shell scripts provided.
+# Column selector for Red Sox data (prints out index and column name):
+curl -s http://www.ccs.neu.edu/home/awjacks/cs3650f18/Labs/2/red_sox_history.csv | awk -F ',' '{ if (NR ==1)  for (i=1; i<=NF; i++){ print i, $i } }'
+
+# Win column from Red Sox historical data:
+curl -s http://www.ccs.neu.edu/home/awjacks/cs3650f18/Labs/2/red_sox_history.csv | awk -F ',' '{ if (NR > 2)  { print $5 } }' > sox_wins.txt
+
+# An alternative win column selector (Now you know about curl and wget)
+wget -O - http://www.mshah.io/comp/Fall18/Systems/Lab/2/baseballdata.csv | cut -d ',' -f 5 | tail -n +2 > data.txt
+
+# There are more public domain texts at the Guttenberg project https://www.gutenberg.org/ 
+# Below are some other examples.
+
+# US Declaration of Independence broken up at each comma:
+curl -s http://www.ccs.neu.edu/home/awjacks/cs3650f18/Labs/2/usdeclar.txt | awk -F ',' '{ if (NR > 6)  for (i=1; i<=NF; i++) { print $i } }' > doi-segment.txt
+
+# US Declaration of Independence word by word:
+curl -s http://www.ccs.neu.edu/home/awjacks/cs3650f18/Labs/2/usdeclar.txt | awk '{ if (NR > 6)  for (i=1; i<=NF; i++) { print $i } }' > doi-word.txt
