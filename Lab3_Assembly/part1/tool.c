a5d4da2569cd44704359eb48424c91f2c6c380b2
erxian <evelynhsu21@gmail.com>
2021-02-11 16:28:55 +0800

    convert instruction to capital

--- Lab3_Assembly/part1/tool.c
+++ Lab3_Assembly/part1/tool.c
@@ -2,17 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, char** argv){
 
-	//char instruList[9] = {"ADD", "SUB", "MUL", "DIV", "MOVE", "LEA", "PUSH", "POP", "RET"}; 
-	char *instruList[9] = {"add", "sub", "mul", "div", "mov", "lea", "pus", "pop", "ret"}; 
-	
-	//printf("argc is the argument count: %d\n", argc);
-	
-	//for(int i=0; i < argc; i++){
-	//	printf("argv[%d] is %s\n", i, argv[i])
-	//}
+	char *instruList[9] = {"ADD", "SUB", "MUL", "DIV", "MOV", "LEA", "PUS", "POP", "RET"}; 
+	//char *instruList[9] = {"add", "sub", "mul", "div", "mov", "lea", "pus", "pop", "ret"}; 
 	
 	FILE* file = fopen(argv[1], "r");
 	size_t len = 255;
@@ -35,14 +30,18 @@ int main(int argc, char** argv){
 	 	char firstThreeChar[3];
 		// get the first 3 characters
 	 	strncpy(firstThreeChar, line+i+1, 3);
+		// convert char to capital
+		for(int i = 0; i < 3; i++){
+			firstThreeChar[i] = toupper(firstThreeChar[i]);
+		}
 		// iterate to check if this line is a valid instrucion
 		for (int i = 0; i < 9; i++){
 	 		if (strcmp(firstThreeChar, instruList[i]) == 0){
 				// MUL has 2 cycles
-				if (strcmp(firstThreeChar, "mul") == 0){
+				if (strcmp(firstThreeChar, "MUL") == 0){
 					total_cycles += 2;
 				// DIV has 4 cycles
-				} else if (strcmp(firstThreeChar, "div") == 0){
+				} else if (strcmp(firstThreeChar, "DIV") == 0){
 					total_cycles += 4;
 				// other instrctions have 1 cycle
 				} else {
