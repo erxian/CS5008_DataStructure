12e665f0c7cea57b506b9a5c6ec51d04ad48829a
erxian <evelynhsu21@gmail.com>
2021-02-12 00:27:14 +0800

    anster questions in barebones.s

--- Lab3_Assembly/part1/barebones.s
+++ Lab3_Assembly/part1/barebones.s
@@ -12,29 +12,29 @@ _barebones:
 main:
 					# (1) What are we setting up here?
 					# Ans:
-	pushq %rbp			#
-	movq  %rsp, %rbp		#
+	pushq %rbp			# push a rbp register to your stack 
+	movq  %rsp, %rbp		# assign the head of the stack to rbp register
 
 					# (2) What is going on here
 					# Ans:
-	movq $1, %rax			# 
-	movq $1, %rdi			#
-	leaq .hello.str,%rsi		#
+	movq $1, %rax			# assign the system call number $1 to rax register  
+	movq $1, %rdi			# set the first paramter 1 to rdi
+	leaq .hello.str,%rsi		# set the second parmater - the address of"Hello World!\n" to rsi
 
 
 					# (3) What is syscall? We did not talk about this
 					# in class.
-					# Ans:
+					# Ans: 
 	syscall				# Which syscall is being run?
-					# Ans:
+					# Ans: sys_write
 
 					# (4) What would another option be instead of 
 					# using a syscall to achieve this?
-					# Ans:
+					# Ans: use printf
 
 	movq	$60, %rax		# (5) We are again setting up another syscall
 	movq	$0, %rdi		# What command is it?
-					# Ans:	
+					# Ans: exit(0)
 	syscall
 
 	popq %rbp			# (Note we do not really need
@@ -43,4 +43,4 @@ main:
 .hello.str:
 	.string "Hello World!\n"
 	.size	.hello.str,13		# (6) Why is there a 13 here?
-					# Ans:	
+					# Ans: the size of "Hello World!\n"
