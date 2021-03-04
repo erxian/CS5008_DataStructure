49324722e7a2120721531eebfc81043d8fa7368e
erxian <evelynhsu21@gmail.com>
2021-03-04 15:48:53 +0800

    finish part1-task6

--- Lab6_MultiprocessingArtists/vfork.c
+++ Lab6_MultiprocessingArtists/vfork.c
@@ -4,23 +4,49 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <math.h>
+
+char colors[64][64*3];
 
 // Modify your paint function here
 void paint(int workID) { 
 	printf("Artist %d is painting\n",workID);
+	for (int i =0; i < 64*3; i++) {
+		colors[workID][i] = rand() % 125;
+	}
+}
+
+
+void save() {
+// Write out the PPM file
+	// If you are looking at this solution, it could
+	// be better laid out in a 'save function'
+	FILE *fp;
+	fp = fopen("vfork.ppm","w+");
+	fputs("P3\n",fp);
+	fputs("64 64\n",fp);
+	fputs("255\n",fp);
+	for(int i =0; i < 64;i++){
+		for(int j =0; j < 64*3; j++){
+			fprintf(fp,"%d",colors[i][j]);
+			fputs(" ",fp);	
+		}
+		fputs("\n",fp);
+	}
+	fclose(fp);
 }
 
 int main(int argc, char** argv){
 
-        int numberOfArtists = 8; // How many child processes do we want?
+        int numberOfArtists = 64; // How many child processes do we want?
 
-	int* integers = malloc(sizeof(int)*8000);
+	//int* integers = malloc(sizeof(int)*8000);
 	pid_t pid;
 	// main loop where we fork new threads
 	int i;
         for (i=0; i < numberOfArtists; i++) {
                 // (1) Perform a fork
-                pid = fork();
+                pid = vfork();
 		
 		
                 // (2) Make only the child do some work (i.e. paint) and then terminate.
@@ -31,17 +57,14 @@ int main(int argc, char** argv){
 			exit(0);
 		}
 
-		pid_t wpid;
-		int status = 0;
-		while ((wpid = wait(&status)) > 0);
-		printf("child pid: %d\n", pid);
+		//pid_t wpid;
+		//int status = 0;
+		//while ((wpid = wait(&status)) > 0);
+		//printf("child pid: %d\n", pid);
         }
 
-//	pid_t wpid;
-//	int status = 0;
-//	while ((wpid = wait(&status)) > 0);
-
         printf("parent is exiting(last artist out!)\n");
-	free(integers);
+	save();
+	//free(integers);
         return 0;
 }
